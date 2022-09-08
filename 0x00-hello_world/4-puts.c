27087379a90427a3d1dab6089948b1b848d71bd5
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-08 14:35:47 -0700

    return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,9 +4,9 @@
  * return: always returns zero
  * 
  */
-int main (void)
+int main(void)
 {
-	puts ("Programming is like building a multilingual puzzle\n");
-	return (0);
+	puts ("\"Programming is like building a multilingual puzzle\n");
+	return(0);
 }
 
