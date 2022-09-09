674dedd1ea7b9413c3d51d698f9dffb9e7797c92
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-09 14:22:36 -0700

    letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ * main - entry point of code
+ *
+ * Return: always returns zero
+ */
+int main(void)
+{
+	char alpha;
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+	return (0);
+}
