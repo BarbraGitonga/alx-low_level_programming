a5f909d931273794a07e29d8b4035df91036cf98
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-09 15:33:17 -0700

    hex

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	int main(void)
+	{
+		int i;
+		char ch;
+
+		for (i = 0; i <= 9; ++i)
+		{
+			putchar('0' + (i));
+		}
+		for (ch = 'a'; ch <= 'f'; ++ch)
+		{
+			putchar(ch);
+			putchar('\n');
+		}
+		return (0);
+	}
+}
