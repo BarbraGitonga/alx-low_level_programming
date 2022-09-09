1f883f13668461c392e98015f2c4f2c451be20d5
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-09 15:04:55 -0700

    digits

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returns zero
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
