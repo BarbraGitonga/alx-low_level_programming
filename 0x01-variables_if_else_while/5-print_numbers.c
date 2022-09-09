8a366e5d316d28923420f826ba0178ce967e2cea
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-09 13:09:41 -0700

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - entry point of code.
+ *
+ * Return: always zero
+ */
+int main(void)
+{
+	for (int a = 0; a < 10; a++)
+	{
+		printf("%d\n", a);
+	}
+	return (0);
+}
