aec5f8b93625abfebf4681bff25ac3ad82ddd6f2
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-10 12:42:03 -0700

    numbers

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+/**
+ * main - rntry point of code
+ *
+ * Return: always return a 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (j = '1'; j <= '9'; j++)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return(0);
+}
+
