0ff549c80e2bdd5f317e02592bc25707665e31f8
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-13 01:25:12 -0700

    signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include "main.h"
+
+/**
+ *print_sign - prints sign of the number
+ *@n: argument to be processed
+ *Return: -1, 0 or +1
+ */
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	return (0);
+}
