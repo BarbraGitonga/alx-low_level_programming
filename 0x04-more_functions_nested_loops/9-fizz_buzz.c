f73bc9ca328acc99a5300050601cec76e0dffcc1
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-16 00:31:39 +0300

    FizzBuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,8 +11,8 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		j = i % 3;
-		k = i % 5;
+		j = (i % 3);
+		k = (i % 5);
 		if (j == 0 && k == 0)
 		{
 			printf("FizzBuzz ");
@@ -21,11 +21,11 @@ int main(void)
 		{
 			printf("Fizz ");
 		}
-		else if (k == 0 && i != 100)
+		else if (k == 0 && i != 100 && j != 0)
 		{
 			printf("Buzz ");
 		}
-		else if (k == 0 && i == 100)
+		else if (k == 0 && i == 100 && j != 0)
 		{
 			printf("Buzz");
 		}
