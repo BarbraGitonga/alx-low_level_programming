6706bb15dc49da3e38cb9c42a57846e16484909b
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-16 00:23:19 +0300

    FizzBuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * main - entrypoint of code
+ *
+ * Return: zero
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 1; i <= 100; i++)
+	{
+		j = i % 3;
+		k = i % 5;
+		if (j == 0 && k == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (j == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (k == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+}
