67f5cc2e21cbc12cee0603acc93b46fdc112449f
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-19 20:10:27 +0300

    revesed string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *rev_string - print a reversed string
+ *@s: string to be reversed
+ *
+ *return: nothing
+ */
+void rev_string(char *s)
+{
+	int i, j;
+	int len;
+	char a;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len = i + 1;
+	}
+	for (j = len; j >= 0; j--)
+	{
+		if (s[j] != '\0')
+		{
+			a = s[j];
+			_putchar(a);
+		}
+	}
+	_putchar('\n');
+}
