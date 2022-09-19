399c3a58f4aadaa48e956d5ae5a9d5fb2bd2eee0
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-19 19:43:48 +0300

    print strings

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * _putchar - a function that prints out a character
+ *
+ * Return: always 0
+ */
+int _putchar(char a)
+{
+        putchar(a);
+        return (0);
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
-_putchar;
+int _putchar(char a);
 void _puts(char *str);
