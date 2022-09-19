1fe8d0b54e650d2d3f527e1b4753ac5feb51fc51
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-19 23:03:12 +0300

    print even words

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ *puts2 - prints every other world in a string
+ *@str: string to be printed
+ *
+ *Return: nothing
+ */
+void puts2(char *str)
+{
+	int i, len, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		len = i + 1;
+	}
+	for (j = 0; j <= len; j + 2)
+	{
+		char a;
+
+		a = str[j];
+		if (a != '\0')
+		{
+			_putchar(a);
+		}
+	}
+}
