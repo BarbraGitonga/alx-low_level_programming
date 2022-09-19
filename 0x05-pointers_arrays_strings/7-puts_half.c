098154110b7592dc089d7b612c15cf669956bab5
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-19 23:43:22 +0300

    half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -8,21 +8,28 @@
  */
 void puts_half(char *str)
 {
-	int i, len, j, len_2;
+	int i, len, j, n;
 	char a;
 
 	for (i = 0; str[i] != '\n'; i++)
 	{
 		len = i;
 	}
-	len_2 = len / 2;
-	for (j = len_2; j <= len; j++)
+	if (len % 2 == 0)
 	{
-		if (j != '\0')
+		for (j = len / 2; str[j] != '\0'; j++)
 		{
 			a = str[j];
 			_putchar(a);
 		}
 	}
+	else
+	{
+		for (n = (len -1) / 2; n < len - 1; n++)
+		{
+			_putchar(str[n + 1]);
+		}
+	}
+
 	_putchar('\n');
 }
