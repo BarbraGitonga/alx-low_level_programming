4c18a982ed8c8998794543776cf148a104bfb0b4
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-19 17:25:00 +0300

    reset to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *reset_to_98 - takes the value of an integr parameter and updates it to 98
+ *@n: pointer to the uinterger value
+ *
+ * Return: nothing
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
