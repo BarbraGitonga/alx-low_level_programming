36099cdf67eee97e0d5009c2594c5c25d704b4c1
Barbra Gitonga <gitongabarbra@gmail.com>
2022-09-25 19:51:10 +0300

    concatenate correction

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,6 @@
 #include "main.h"
+#include <stddef.h>
+
 
 /**
  * _strcat - concatenates two strings
@@ -6,22 +8,25 @@
  * @src: string to add
  * Return: pointer to the resulting string
  */
-
 char *_strcat(char *dest, char *src)
 {
-	int i = 0, j = 0;
+	int i, j;
 
+	i = 0;
+	j = 0;
 	while (dest[i] != '\0')
-		i++;
-
-	while (src[j] != '\0')
 	{
-		dest[i] = src[j];
-		j++;
+		dest[j] = dest[i];
 		i++;
+		j++;
 	}
-
-	dest[i] = '\0';
-
+	i = 0;
+	while (src[i] != '\0')
+	{
+		dest[j] = src[i];
+		i++;
+		j++;
+	}
+	dest[j] = '\0';
 	return (dest);
 }
