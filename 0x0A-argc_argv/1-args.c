b425c30a30b0ca254a270c81800aae966bbdcf83
Barbra Gitonga <gitongabarbra@gmail.com>
2022-10-01 22:30:19 +0300

    correction to betty format

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,8 +6,8 @@
  *@argv: strings in the argument
  *Return: always 0
  */
-int main(int argc, char **argv __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d \n", argc-1);
+	printf("%d \n", argc - 1);
 	return (0);
 }
