e9f948773c963b89629a82565f44a706a0a99970
Barbra Gitonga <gitongabarbra@gmail.com>
2022-10-01 22:32:21 +0300

    correction to betty format

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,13 +1,15 @@
-#include "main.h"
 #include <stdio.h>
+
 /**
- *main - counts number of argument in terminal
- *@argc: number of arguments in terminal
- *@argv: strings in the argument
- *Return: always 0
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0 (Success)
  */
+
 int main(int argc, char *argv[] __attribute__((unused)))
 {
-	printf("%d \n", argc - 1);
+	printf("%d\n", argc - 1);
 	return (0);
 }
