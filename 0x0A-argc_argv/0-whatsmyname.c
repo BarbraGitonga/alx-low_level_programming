b9c51683243f2ea8a228c50e220165e08d59952e
Barbra Gitonga <gitongabarbra@gmail.com>
2022-10-01 20:37:38 +0300

    fixing error in betty formart

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,14 +4,14 @@
  *main - prints the name of the program
  *
  *@argc: this is the value of the total number of elements in a string
- *@*argv: this is holds the strings
+ *@argv: this is holds the strings
  *Return: always 0
  */
 int main(int argc, char *argv[])
 {
 	if (argc > 0)
 	{
-		printf("%s\n",argv[0]);
+		printf("%s\n", argv[0]);
 	}
 	return (0);
 }
