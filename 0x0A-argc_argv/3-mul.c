0a4919c40107225f5b77ca88aad1249c6ffc1e72
Barbra Gitonga <gitongabarbra@gmail.com>
2022-10-01 21:30:51 +0300

    multiply the numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ *main - muktiplues two number sin the terminal.
+ *@argc: contaings number of arguments.
+ *@string fo arguments.
+ *Return: 0 if multiplied nd 1 if there is an error
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 3)
+	{
+		printf("%d \n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else if (argc != 3)
+	{
+		printf("Error \n");
+		return (1);
+	}
+	return (0);
+}
