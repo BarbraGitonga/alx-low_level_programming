66eb847dd0bc6b3eff4458883c7deb433684ba12
Barbra Gitonga <gitongabarbra@gmail.com>
2022-10-01 21:08:34 +0300

    prints out strings of the argument in terminal

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *main - print out a string of words terminal arguments.
+ *@argc: the total number of arguments.
+ *@argv: this hild the strings the terminal.
+ *Return: This returns 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s \n", argv[i]);
+	}
+	return (0);
+}
