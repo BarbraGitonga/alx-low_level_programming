ee44bf55a22b6bb294d9626b94da836ce24c51e0
Barbra Gitonga <gitongabarbra@gmail.com>
2022-10-01 20:35:39 +0300

    print out the command only

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ *main - prints the name of the program
+ *
+ *@argc: this is the value of the total number of elements in a string
+ *@*argv: this is holds the strings
+ *Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n",argv[0]);
+	}
+	return (0);
+}
