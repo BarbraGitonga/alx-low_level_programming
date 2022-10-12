0c6200297e471f87ec716099201064d212470df0
Barbra Gitonga <gitongabarbra@gmail.com>
2022-10-12 12:30:57 +0300

    doggo

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - hold description a dog
+ *@name: name of dog.
+ *@age: age of dog.
+ *@owner: name of the owner of the dog.
+ *
+ *Description: This structure holds information about pet dogs.
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
