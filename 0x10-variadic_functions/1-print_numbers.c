862ad0d3c614a14ed0b2634935d0e247d2f226b6
Barbra Gitonga <gitongabarbra@gmail.com>
2022-10-19 23:51:30 +0300

    prints numbers and corrected bettu pf sum

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,6 +1,35 @@
-#include <stdio.h>
-#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ *print_numbers - prints numbers stored in function
+ *
+ *@separator:string to be printed between numbers.
+ *@n: number of elements to be stored
+ *Return: void
+ */
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+	va_list num;
 
+	va_start(num, n);
+
+	for (i = 0; i < n; i++)
+	{
+		if (!separator)
+		{
+			printf("%d", va_arg(num, int));
+		}
+		else if (separator && i == 0)
+		{
+			printf("%d", va_arg(num, int));
+		}
+		else
+		{
+			printf("%s%d", separator, va_arg(num, int));
+		}
+	}
+	va_end(num);
+	putchar('\n');
+}
